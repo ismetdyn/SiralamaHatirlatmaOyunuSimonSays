dec43db62cf6a972e27813e24a14155963bc0d50
ismet <ismet@ABRAA5>
2022-02-23 17:26:06 +0300

    Proje dosyası ekle.

---
+++ Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿
+namespace Proje5._40_SiralamaHatirlatmaOyunuSimonSays
+{
+    partial class frmSiralamaHatirlatma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lDurum = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Location = new System.Drawing.Point(13, 13);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(148, 56);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(268, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Durum:";
+            // 
+            // lDurum
+            // 
+            this.lDurum.AutoSize = true;
+            this.lDurum.Location = new System.Drawing.Point(330, 33);
+            this.lDurum.Name = "lDurum";
+            this.lDurum.Size = new System.Drawing.Size(11, 16);
+            this.lDurum.TabIndex = 2;
+            this.lDurum.Text = "-";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button20);
+            this.groupBox1.Controls.Add(this.button19);
+            this.groupBox1.Controls.Add(this.button18);
+            this.groupBox1.Controls.Add(this.button17);
+            this.groupBox1.Controls.Add(this.button13);
+            this.groupBox1.Controls.Add(this.button16);
+            this.groupBox1.Controls.Add(this.button15);
+            this.groupBox1.Controls.Add(this.button14);
+            this.groupBox1.Controls.Add(this.button12);
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.button10);
+            this.groupBox1.Controls.Add(this.button9);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button5);
+            this.groupBox1.Controls.Add(this.button4);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 101);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(535, 291);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Butonlar";
+            // 
+            // button20
+            // 
+            this.button20.Location = new System.Drawing.Point(411, 214);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(95, 55);
+            this.button20.TabIndex = 19;
+            this.button20.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(310, 214);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(95, 55);
+            this.button19.TabIndex = 18;
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(209, 214);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(95, 55);
+            this.button18.TabIndex = 17;
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(108, 214);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(95, 55);
+            this.button17.TabIndex = 16;
+            this.button17.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(7, 214);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(95, 55);
+            this.button16.TabIndex = 15;
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(411, 153);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(95, 55);
+            this.button15.TabIndex = 14;
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(310, 153);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(95, 55);
+            this.button14.TabIndex = 13;
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(209, 153);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(95, 55);
+            this.button13.TabIndex = 12;
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(108, 153);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(95, 55);
+            this.button12.TabIndex = 11;
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(7, 153);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(95, 55);
+            this.button11.TabIndex = 10;
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(411, 92);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(95, 55);
+            this.button10.TabIndex = 9;
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(310, 92);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(95, 55);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(209, 92);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(95, 55);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(108, 92);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(95, 55);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(7, 92);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(95, 55);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(411, 31);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(95, 55);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(310, 31);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 55);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(209, 31);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 55);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(108, 31);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 55);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(7, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 55);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonlar_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // frmSiralamaHatirlatma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 425);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lDurum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBasla);
+            this.Name = "frmSiralamaHatirlatma";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frmSiralamaHatirlatma_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lDurum;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proje5._40_SiralamaHatirlatmaOyunuSimonSays
+{
+    public partial class frmSiralamaHatirlatma : Form
+    {
+        public frmSiralamaHatirlatma()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            sayi++;
+            secilenSira = "";
+            lDurum.Text = "";
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        int sayi = 0;
+        string gercekSira = "", secilenSira = "";
+
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for(int i= 0; i < sayi; i++)
+            {
+                Control ctrl = groupBox1.Controls["button" + gercekSira.Split(',')[i]];
+                ctrl.Text = Convert.ToString(i + 1);
+                System.Threading.Thread.Sleep(1000);
+            }
+            butonlariTemizle();
+        }
+        private void frmSiralamaHatirlatma_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            gercekSira = ListeKaristir();
+        }
+
+
+        private string ListeKaristir()
+        {
+            ArrayList dizi = new ArrayList();
+            for(int i = 0; i < groupBox1.Controls.Count; i++)
+            {
+                dizi.Add(i.ToString());
+            }
+            string sayilar = "";
+            Random r = new Random();
+            while(dizi.Count > 0)
+            {
+                int rastgele = r.Next(0, dizi.Count);
+                sayilar += dizi[rastgele] + ",";
+                dizi.RemoveAt(rastgele);
+            }
+            return sayilar;
+        }
+        private void buttonlar_Click(object sender, EventArgs e)
+        {
+            secilenSira += (sender as Button).Name.ToString().Replace("button", "") + ",";
+            if (gercekSira.Substring(0, secilenSira.Length) == secilenSira)
+            {
+                lDurum.Text = "doğru seçim";
+            }
+            else
+            {
+                lDurum.Text = "YANLIŞ yaptınız ve yandınız.";
+                groupBox1.Enabled = false;
+                for(int i= 0; i < groupBox1.Controls.Count; i++)
+                {
+                    Control ctrl = groupBox1.Controls["button" + gercekSira.Split(',')[i]];
+                    ctrl.Text = Convert.ToString(i + 1);
+                }
+            }
+        }
+
+        private void butonlariTemizle()
+        {
+            for(int i = 1; i <= groupBox1.Controls.Count; i++)
+            {
+                Control ctrl= groupBox1.Controls["button" + i];
+                ctrl.Text = "";
+            }
+        }
+    }
+}
